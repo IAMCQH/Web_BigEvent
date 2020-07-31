b65641575611463d0c0b2b178bed5c6252ff44ac
IAMCQH <908247643@qq.com>
2020-07-31 16:09:02 +0800

    完成了登录和注册的功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
 $(function() {
-
+    //监听表单登录
+    var layer = layui.layer
+    $("#form_login").on("submit", function(e) {
+        e.preventDefault()
+        $.ajax({
+            type: "POST",
+            url: "/api/login",
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg("登陆成功")
+                localStorage.setItem("token", res.token)
+                location.href("/index.html")
+            }
+        })
+    })
 })
